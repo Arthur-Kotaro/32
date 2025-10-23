c660c986b228a2b458914505646180cdfe8e666b
arthur-kotaro <arthur.kotaro@yandex.ru>
2025-10-23 11:37:56 +0400

    Task2: json file add

--- 2/src/main2.cpp
+++ 2/src/main2.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "main2.hpp"
+#include "nlohmann/json.hpp"
+
 //#define DEBUG
 
 int main()
 {
+    std::ifstream file("Films.json");
+    nlohmann::json dict;
+    file >> dict;
+    
 #ifdef DEBUG
     std::cout<< "DEBUG: ...\n";
 #endif
